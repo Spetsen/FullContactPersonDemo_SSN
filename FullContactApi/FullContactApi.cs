e7b3888e49163372916ea38bbd602d3ddda3c5bd
Simon Spetz-Nyström <simon.spetz-nystrom@telia.com>
2015-04-22 19:42:12 +0200

    Fix for synchronous operation in FullContactApi. Removal of unused file.

--- FullContactApi/FullContactApi.cs
+++ FullContactApi/FullContactApi.cs
@@ -7,10 +7,12 @@ namespace FullContactApi
     public class FullContactApi : IFullContactApi
     {
         private readonly string mApiKey;
+        private readonly TaskFactory mTaskFactory;
 
         public FullContactApi(string apiKey)
         {
             mApiKey = apiKey;
+            mTaskFactory = new TaskFactory();
         }
 
         public async Task<FullContactPerson> LookupPersonByEmailAsync(string email)
@@ -20,7 +22,8 @@ namespace FullContactApi
             string requestUri = "https://api.fullcontact.com/v2/person.json?email=" + email + "&apiKey=" + mApiKey;
             var res = await client.GetAsync(requestUri);
             var json = await res.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<FullContactPerson>(json);
+            var retValTask = mTaskFactory.StartNew(() => JsonConvert.DeserializeObject<FullContactPerson>(json));
+            return await retValTask;
         }
     }
 }
